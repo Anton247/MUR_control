748ca87978170b1f20f717671350579354f895ad
Anton <kondratov247@mail.ru>
2022-05-23 18:39:25 +0700

    Update lesson_8_1.py

--- lesson_8_1.py
+++ lesson_8_1.py
@@ -1,3 +1,4 @@
+from decimal import DivisionByZero
 from cv2 import circle, moments
 import pymurapi as mur
 import time
@@ -88,10 +89,48 @@ def find_yellow_circle(img):
                     if rectangle_area > circle_area:
                         moments = cv2.moments(c)
                         try:
-                            
-while True:
-    keep_depth(2)
-    keep_yaw(60, 30)
+                            x = int(moments["m10"] / moments["m00"])
+                            y = int(moments["m01"] / moments["m00"])
+                            return True, x, y
+                        except DivisionByZero:
+                            return False, 0, 0
+                    else:
+                        continue
+                else:
+                    continue
+    return False, 0, 0
+
+def stab_on_yellow_circle(image):
+    found, x, y = find_yellow_circle(image)
+    if found:
+        x_center = x - 320/2
+        y_center = y - 240/2
+    
+        try:
+            output_forward = stab_on_yellow_circle.regulator_forward.process(y_center)
+            output_side = stab_on_yellow_circle.regulator_side.process(x_center)
+            output_side = clamp(output_side, -50, 50)
+            output_forward = clamp(output_forward, -50, 50)
+            auv.set_motor_power(0, -output_forward)
+            auv.set_motor_power(1, -output_forward)
+
+            auv.set_motor_power(4, -output_side)
+
+        except AttributeError:
+            stab_on_yellow_circle.regulator_forward = PD()
+            stab_on_yellow_circle.regulator_forward.set_p_gain(0.8)
+            stab_on_yellow_circle.regulator_forward.set_d_gain(0.5)
+
+            stab_on_yellow_circle.regulator_side = PD()
+            stab_on_yellow_circle.regulator_side.set_p_gain(0.8)
+            stab_on_yellow_circle.regulator_side.set_d_gain(0.5)
+
+
+
+
+while True: 
+    image = auv.get_image_bottom()
+    stab_on_yellow_circle(image)
     time.sleep(0.03)
     
     
